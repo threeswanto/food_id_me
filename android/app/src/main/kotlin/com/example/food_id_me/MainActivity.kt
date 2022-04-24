172202d7ad987f6a78a86c3ac082a339ec8a9c19
tris_yoko <61571887+threeswanto@users.noreply.github.com>
2022-04-24 19:26:09 +0700

    Initial project food id me

---
+++ android/app/src/main/kotlin/com/example/food_id_me/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.food_id_me
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:food_id_me/home.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(),
+    );
+  }
+}
