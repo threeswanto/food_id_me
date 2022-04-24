0e7084377e0a197def5025dd071e44636f8f8a65
tris_yoko <61571887+threeswanto@users.noreply.github.com>
2022-04-24 19:46:50 +0700

    Add response_repository.dart

---
+++ lib/repository/response_repository.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:food_id_me/base/api_endpoint.dart';
+import 'package:food_id_me/model/response.dart';
+import 'package:http/http.dart' as http;
+
+class ResponseRepository {
+  void doGetBanner(String token, onSuccess, onError) async {
+    try {
+      var url = APIEndpoint.baseUrl + APIEndpoint.banner;
+      var uri = Uri.parse(url);
+      var response = await http.get(
+        uri,
+      );
+      Response apiResponse = Response.fromJson(json.decode(response.body));
+      onSuccess(apiResponse);
+    } on SocketException {
+      onError('No Internet');
+    } on HttpException {
+      onError('No Service Found');
+    } on FormatException {
+      onError('Invalid Response Format');
+    } catch (e) {
+      onError(e.toString());
+    }
+  }
+}
