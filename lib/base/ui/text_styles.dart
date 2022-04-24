a85e5212294b2389b063954e6bb8ce761bee1876
tris_yoko <61571887+threeswanto@users.noreply.github.com>
2022-04-24 21:31:48 +0700

    Add text_style.dart

---
+++ lib/base/ui/text_styles.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+const TextStyle text18Bold = TextStyle(fontSize: 18, fontWeight: FontWeight.bold);
+const TextStyle text16Bold = TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+const TextStyle text14Bold = TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
+
+const TextStyle text18Black = TextStyle(fontSize: 18,  color: Colors.black);
+const TextStyle text16Black = TextStyle(fontSize: 16,  color: Colors.black);
+const TextStyle text14Black = TextStyle(fontSize: 14,  color: Colors.black);
+
+const TextStyle text18WhiteBold = TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white);
+const TextStyle text16WhiteBold = TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white);
+const TextStyle text14WhiteBold = TextStyle(fontSize: 14, fontWeight: FontWeight.bold, color: Colors.white);
+
+const TextStyle text18White = TextStyle(fontSize: 18, color: Colors.white);
+const TextStyle text16White = TextStyle(fontSize: 16, color: Colors.white);
+const TextStyle text14White = TextStyle(fontSize: 14, color: Colors.white);
